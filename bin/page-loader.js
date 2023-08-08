452c6de4fc51bea8265c33e97bf0ec64c2587280
Sergei Kiselev <kiselev.s.d.98@gmail.com>
2023-08-08 03:53:44 +0300

    add and set commander.js

--- bin/page-loader.js
+++ bin/page-loader.js
@@ -1,4 +1,15 @@
 #!/usr/bin/env node
+import { program } from 'commander'; // eslint-disable-line
 import app from '../src/index.js';
 
-app();
+program
+  .description('Page loader utility')
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .option('-o, --output [dir]', 'output dir', '/home/user/current-dir')
+  .arguments('<url>')
+  .action((url, options) => {
+    app(url);
+    console.log(options);
+  });
+
+program.parse();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-export default () => {
-  console.log('hello');
+export default (url) => {
+  console.log(url);
 };
