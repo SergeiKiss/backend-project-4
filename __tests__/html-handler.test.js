60694d98d821a64ddfd0af31840d92d4e7ec626f
Sergei Kiselev <kiselev.s.d.98@gmail.com>
2023-08-10 05:10:39 +0300

    add new tests for html-handler.js

--- __tests__/html-handler.test.js
+++ __tests__/html-handler.test.js
@@ -2,13 +2,10 @@ import fs from 'fs/promises';
 import os from 'os';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { beforeEach, test, expect, afterAll, jest } from '@jest/globals'; // eslint-disable-line
-import nock from 'nock'; // eslint-disable-line
-import loadPage from '../src/index.js';
+import { beforeEach, test, expect, afterAll } from '@jest/globals'; // eslint-disable-line
+import extractFilesAndPrepareHTML from '../src/helpers/html-handler.js';
 
-// loadPage(url, outputPath)
-
-nock.disableNetConnect();
+// extractFilesAndPrepareHTML(rawHTML, dir)
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -29,21 +26,9 @@ beforeEach(async () => {
   expectedHTML = await fs.readFile(afterPath, 'utf-8');
 });
 
-test('page-loader - basic case', async () => {
-  const url = new URL('https://ru.hexlet.io/courses');
-  nock(url.origin)
-    .get(url.pathname)
-    .reply(200, rawHTML);
-
-  const logSpy = jest.spyOn(console, 'log');
-
-  await loadPage(url.href, currentDir);
-
-  const expectedMainFilePath = `${currentDir}/ru-hexlet-io-courses.html`;
-  const actualData = await fs.readFile(expectedMainFilePath, { encoding: 'utf8' });
-
-  expect(actualData).toEqual(expectedHTML);
-  expect(logSpy).toHaveBeenCalledWith(expectedMainFilePath);
+test('', async () => {
+  const preparedHTML = await extractFilesAndPrepareHTML(rawHTML);
+  expect(preparedHTML).toEqual(expectedHTML);
 
   const expectedFilesDirPath = path.resolve(currentDir, 'ru-hexlet-io-courses_files');
   await fs.opendir(expectedFilesDirPath)
