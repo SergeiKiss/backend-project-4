62515f3b2f6dc48ee85432d664fdbece48c2cfd2
Sergei Kiselev <kiselev.s.d.98@gmail.com>
2023-08-10 05:34:36 +0300

    single out a separate func - createName

--- src/helpers/common-funcs.js
+++ src/helpers/common-funcs.js
@@ -1,4 +1,9 @@
-export default (str) => {
+export const replaceSymbols = (str) => {
   const reg = /[^a-zа-яё\d]/g;
   return str.replaceAll(reg, '-');
+};
+
+export const createName = (str, ending) => {
+  const name = `${replaceSymbols(str)}${ending}`;
+  return name;
 };
--- src/helpers/writeFile.js
+++ src/helpers/writeFile.js
@@ -1,10 +1,10 @@
 import fs from 'fs/promises';
 import path from 'path';
-import replaceSymbols from './common-funcs.js';
+import { createName } from './common-funcs.js';
 
 export default (url, dir, data) => {
   const { hostname, pathname } = new URL(url);
-  const fileName = `${replaceSymbols(`${hostname}${pathname}`)}.html`;
+  const fileName = createName(`${hostname}${pathname}`, '.html');
   const filePath = path.resolve(dir, fileName);
   return fs.writeFile(filePath, data)
     .then(() => filePath)
