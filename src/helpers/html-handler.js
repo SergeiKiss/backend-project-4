0d6b8f04d7b380f2095ddd16381859a3736044cc
Sergei Kiselev <kiselev.s.d.98@gmail.com>
2023-08-12 13:40:31 +0300

    implement load of links and scripts

--- src/helpers/html-handler.js
+++ src/helpers/html-handler.js
@@ -4,28 +4,43 @@ import * as cheerio from 'cheerio'; // eslint-disable-line
 import load from './load.js';
 import { createName } from './common-funcs.js';
 
-const extractFilesAndPrepareHTML = (url, dirPath, rawHTML) => {
+const loadFilesAndChangeAttrs = (elements, outputDir, url, $, attrName) => {
+  for (let i = 0; i < elements.length; i += 1) {
+    const { hostname } = new URL(url);
+    const el = $(elements[i]);
+    const attr = el.attr(attrName);
+    const elURL = new URL(attr, url);
+    const { hostname: elHostname, pathname: elPathname } = elURL;
+    if (hostname === elHostname) {
+      const { dir, ext, name } = path.parse(`${elHostname}${elPathname}`);
+      const elName = createName(`${dir}/${name}`, ext || '.html');
+      const responseType = ext === '' ? 'json' : 'stream';
+      const elPath = path.resolve(outputDir.dirFilesPath, elName);
+      el.attr(attrName, path.join(outputDir.dirFilesName, elName));
+      load(elURL, responseType)
+        .then((data) => {
+          fs.writeFile(elPath, data);
+        });
+    }
+  }
+};
+
+const extractFilesAndPrepareHTML = (url, outputDirPath, rawHTML) => {
   const { hostname, pathname } = new URL(url);
   const dirFilesName = createName(`${hostname}${pathname}`, '_files');
-  const dirFilesPath = path.resolve(dirPath, dirFilesName);
+  const dirFilesPath = path.resolve(outputDirPath, dirFilesName);
   const $ = cheerio.load(rawHTML);
   return fs.mkdir(dirFilesPath)
     .then(() => {
       const images = $('img');
-      for (let i = 0; i < images.length; i += 1) {
-        const imgEl = $(images[i]);
-        const src = imgEl.attr('src');
-        const imgURL = new URL(src, url);
-        const { hostname: imgHostname, pathname: imgPathname } = new URL(imgURL);
-        const { dir, ext, name } = path.parse(`${imgHostname}${imgPathname}`);
-        const imgName = createName(`${dir}/${name}`, ext);
-        const imgPath = path.resolve(dirFilesPath, imgName);
-        imgEl.attr('src', path.join(dirFilesName, imgName));
-        load(imgURL, 'stream')
-          .then((data) => {
-            fs.writeFile(imgPath, data);
-          });
-      }
+      loadFilesAndChangeAttrs(images, { dirFilesPath, dirFilesName }, url, $, 'src');
+
+      const links = $('link');
+      loadFilesAndChangeAttrs(links, { dirFilesPath, dirFilesName }, url, $, 'href');
+
+      const scripts = $('script');
+      loadFilesAndChangeAttrs(scripts, { dirFilesPath, dirFilesName }, url, $, 'src');
+
       return $.html();
     });
 };
